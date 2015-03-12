d1f8a6f0e2b84e51f919574bca605654f87f9cb3
psyriccio <psyriccio@PROGRAM1.forteholding.ru>
2015-03-12 17:08:35 +0300

    main http routing skeleton

--- src/main/scala/mokko/flex/MainRoute.scala
+++ src/main/scala/mokko/flex/MainRoute.scala
@@ -6,8 +6,52 @@
 
 package mokko.flex
 
+import com.google.common.io.Files
+import java.io.File
+import spray.http.HttpData
+import spray.http.HttpEntity
+import spray.http.MediaType
+import spray.http.MediaTypes._
 import spray.routing.Directives
+import spray.routing.Route
 
 trait MainRoute extends Directives with AppLogging {
 
+  val MainRoute: Route = {
+    
+    def path_(value: String) = {
+      (pathPrefix(value) & pathEndOrSingleSlash)
+    }
+    
+   def static(urlPath: String, fromPath: String) = {
+      path(urlPath / Rest) { pathRest =>
+        log.info(s"GET ${urlPath}/${pathRest}")
+        def result(mediaType: MediaType) = {
+          respondWithMediaType(mediaType) {
+            complete {
+              val source = Files.toByteArray(new File(s"${fromPath}/${pathRest}"))
+              val entity = HttpEntity(HttpData(source))
+              entity
+            }
+          }
+        }
+        val suff = pathRest.substring(pathRest.size-3)
+        suff match {
+          case "css" => result(`text/css`)
+          case ".js" => result(`application/javascript`) 
+          case _ => result(`application/octet-stream`)
+        }
+      }
+    }
+    get {
+      //static("css", "../../scala-2.11/resource_managed/main/css") ~
+      //static("pub", "../../../web") ~
+      //static("js", "../../scala-2.11") ~
+      path_("") {
+        complete {
+          s"Flex-server ${buildinfo.buildInfo.name} ${buildinfo.buildInfo.version}"
+        }
+      }
+    }    
+  }
 }
