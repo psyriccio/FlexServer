24bbfdc9a153a45785b9a9f7a9401bfa5f93c459
psyriccio <psyriccio@PROGRAM1.forteholding.ru>
2015-03-17 17:37:26 +0300

    * fix: loging unknown parts in POST

--- src/main/scala/mokko/flex/MainRoute.scala
+++ src/main/scala/mokko/flex/MainRoute.scala
@@ -102,7 +102,8 @@ trait MainRoute extends Directives with AppLogging {
             }
           } ~
           post {
-            log.info(s"POST ${requestUri.toString}")
+            log.info(s"POST ${requestUri.toString()}")
+            log.debug(entity(as[AnyRef]).toString())
             entity(as[MultipartFormData]) { formData =>
               log.info(s"formData = ${formData.fields.mkString}")
               complete {
