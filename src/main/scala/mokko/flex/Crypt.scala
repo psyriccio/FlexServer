19824f4af27a50b055e9d4e3a68a532bec544a3e
psyriccio <psyriccio@PROGRAM1.forteholding.ru>
2015-03-19 14:29:58 +0300

    + sha256 for secret

--- src/main/scala/mokko/flex/Crypt.scala
+++ src/main/scala/mokko/flex/Crypt.scala
@@ -12,22 +12,22 @@ import java.security.MessageDigest
  
 object Sha256 {
   private val sha = MessageDigest.getInstance("SHA-256")
-  def digest(s: String): String = {
-    new String(sha.digest(s.getBytes), "UTF-8")
+  def digest(s: String): Array[Byte] = {
+    sha.digest(s.getBytes)
   }
 }
 
 class Crypt(algorithmName: String) {
   
   def encrypt(bytes: Array[Byte], secret: String): Array[Byte] = {
-    val secretKey = new SecretKeySpec(secret.getBytes("UTF-8"), algorithmName)
+    val secretKey = new SecretKeySpec(Sha256.digest(secret), algorithmName)
     val encipher = Cipher.getInstance(algorithmName + "/ECB/PKCS5Padding")
     encipher.init(Cipher.ENCRYPT_MODE, secretKey)
     encipher.doFinal(bytes)
   }
  
   def decrypt(bytes: Array[Byte], secret: String): Array[Byte] = {
-    val secretKey = new SecretKeySpec(secret.getBytes("UTF-8"), algorithmName)
+    val secretKey = new SecretKeySpec(Sha256.digest(secret), algorithmName)
     val encipher = Cipher.getInstance(algorithmName + "/ECB/PKCS5Padding")
     encipher.init(Cipher.DECRYPT_MODE, secretKey)
     encipher.doFinal(bytes)
